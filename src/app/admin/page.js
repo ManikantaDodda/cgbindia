4734b3933f27a4c59fb262f3fdc93b32cfbee046
ManikantaDodda <nagasaidodda@gmail.com>
2024-09-21 00:07:32 +0530

    admin

---
+++ src/app/admin/page.js
@@ -0,0 +1,27 @@
+"use client";
+import { useSession } from 'next-auth/react';
+import Navbar from '@/components/Navbar';
+import { redirect } from 'next/navigation';
+
+export default async function AdminPanel() {
+  const {data : session, status} =  useSession();
+
+  // Redirect if the user is not authenticated or not an admin
+  if (!session || session.user.role !== 'admin') {
+    redirect('/login');
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <div className="flex flex-col items-center justify-center py-8 bg-gray-100">
+        <h1 className="text-3xl font-bold text-center mb-4">Admin Panel</h1>
+        <Link href="/dashboard">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Back to Dashboard
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
