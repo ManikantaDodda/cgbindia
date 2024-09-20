8c603ef2e4ace600a73131eee9501f83a6fb9db0
ManikantaDodda <nagasaidodda@gmail.com>
2024-09-20 23:19:58 +0530

    mongodb conn

--- src/database/index.js
+++ src/database/index.js
@@ -5,11 +5,13 @@ const configOptions = {
 };
 
 const connectToDB = async () => {
+  const atlasDB = `mongodb+srv://doddamanikanta:Mani@clusterx.uk5pb8j.mongodb.net/cgbindia?retryWrites=true&w=majority&appName=ClusterX`; 
+ 
   const connectionUrl =
     "mongodb://localhost:27017/cgbindia";
 
   mongoose
-    .connect(connectionUrl, configOptions)
+    .connect(atlasDB, configOptions)
     .then(() => console.log("database connected successfully!"))
     .catch((err) =>
       console.log(`Getting Error from DB connection ${err.message}`)
